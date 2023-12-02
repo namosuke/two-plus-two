b5f89b1900c5f06cca7d650636e29a9ccc7b22b5
ナモすけ <dynamorollerbot@gmail.com>
2023-12-03 05:45:09 +0900

    add build directory

---
+++ _fresh/chunk-QKUHNQW3.js
@@ -0,0 +1,1 @@
+import{g as r}from"./chunk-BV6DNIGX.js";var s=r([]);var d=r(void 0);function n(t){return t.children}n.displayName="Partial";
---
+++ _fresh/island-bigbrother.js
@@ -0,0 +1,1 @@
+import{a as e}from"./chunk-BRQEXE2C.js";import"./chunk-BV6DNIGX.js";function r(i){return e("div",{class:`overflow-hidden b-b b-b-${i.signal.value}`,children:e("img",{src:"/big-brother.png",alt:`Big Brother is ${i.signal.value}`,class:"w-96 max-w-full mx-auto",title:`Big Brother is ${i.signal.value}`})})}export{r as default};
